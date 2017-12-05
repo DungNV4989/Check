9254e557970392daa2fdd56ae3eb87e701a9101a
NGUYEN VAN DUNG <dungnv4989@gmail.com>
2017-12-05 08:47:13 -0800

    basic login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
+<<<<<<< HEAD
 	def full_title(page_title = '')
+=======
+	def full_title(page_title = "")
+>>>>>>> basic login
 		base_title = t "application_helper.base_title"
 		if page_title.empty?
 			base_title
